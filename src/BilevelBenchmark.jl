92e7219acfecddc0702098e7256868806b5a7681
Valentin Dijon <valentin.dijon@free.fr>
2025-06-25 17:41:06 -0400

    Linear BOLIB problems added AND all tested and passed the tests !

--- src/BilevelBenchmark.jl
+++ src/BilevelBenchmark.jl
@@ -38,7 +38,7 @@ function BiJuMP_convertor(prob::BilevelProblem)
     return model
 end
 
-all_probs = collect(1:138) # List of problem numbers to test
+all_probs = collect(1:162) # List of problem numbers to test
 JuMP_uncompatible = [3, 12, 14, 15, 16, 17, 18, 21, 32, 33, 34, 36, 40, 41, 42, 43, 49, 50, 51, 52, 53, 54, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 79, 80, 81, 83, 84, 85, 86, 87, 88, 94, 95, 96, 97, 98, 99, 100, 101, 103, 104, 109, 112, 113, 114, 115, 117, 119, 120, 123, 124, 126, 127, 129, 130, 131, 132] # Problems that are not compatible with JuMP
 
 prob_numbers = filter(x -> !(x in JuMP_uncompatible), all_probs) # Filter out incompatible problems
